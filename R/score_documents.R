c36f9cabf45538466ea85c1359afbfcce3816d65
Will <wilryh@gmail.com>
2018-12-09 13:02:02 -0500

    Add document length correction

--- R/score_documents.R
+++ R/score_documents.R
@@ -8,8 +8,12 @@
 #' \code{score_documents} uses word scores to assign document scores
 #'
 #' @param scores A list from output of \code{scale_text}
-#' @param n_dimensions An integer scalar, How many dimensions of scaled text to
+#' @param n_dimensions An integer scalar. How many dimensions of scaled text to
 #' score.
+#' @param doc_length_correction A numeric scalar. Power on document length:
+#' document_scores / document_length^(doc_length_correction). Set so
+#' that extremity on top dimensions is not strongly correlated with document
+#' length.
 #'
 #' @examples
 #'
@@ -29,7 +33,10 @@
 #' \code{\link{get_keywords}}, \code{\link{plot_keywords}},
 #' \code{\link{doc_to_tdm}}
 
-score_documents <- function(scores, n_dimensions = 10)
+score_documents <- function(
+                            scores, n_dimensions = 10,
+                            doc_length_correction = 0.75
+                            )
     {
 
         scores$tdm <- scores$tdm_orig[,colnames(scores$tdm_orig) %in%
@@ -46,7 +53,8 @@ score_documents <- function(scores, n_dimensions = 10)
         ## standardize by number of words
         scored <- as.matrix(
             scored
-            ) / n_words
+        ) / n_words^(doc_length_correction) # abs(X1)/abs(X2) should not be
+                                            # strongly correlated w doc length
 
         ## convert document scores to data frame
         scored <- data.frame(scored)
